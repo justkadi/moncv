dd48d9e8fc948808e5e5e7d3d2b6b8410f07eb23
Kadi <kadidiatou.diallo@isfsc.be>
2023-11-19 14:24:55 +0100

    debut

---
+++ formation.php
@@ -0,0 +1,5 @@
+<h3>Formations</h3>
+    <ul>
+      <li>2020-2021 : 1er Bachelier écriture multimèdia, Haute École ISFSC sur Schaerbeek</li>
+      <li>2022-2023 : 2er Bachelier écriture multimèdia, Haute École ISFSC sur Schaerbeek</li>
+    </ul>
